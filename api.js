9fc6a3976d0046ff4356614626ae4563aead42d7
Yoqub573 <yokub573@gmail.com>
2025-05-25 11:58:51 +0500

    adas

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 import getData from "./dom.js";
-
 let api = "https://6821ee2fb342dce8004c65e1.mockapi.io/dataUsers"
+
 export async function get() {
     try {
         let responce = await fetch(api)
--- dom.js
+++ dom.js
@@ -1,4 +1,5 @@
 let box = document.querySelector(".box");
+
 export default function getData(data) {
   box.innerHTML = "";
   data.forEach((e) => {
